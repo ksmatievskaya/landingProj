a2ff3a3d430c385b5913ae978d1d35e78836db6f
ksmatievskaya <ksmatievskaya@gmail.com>
2022-09-04 12:13:45 +0300

    edited gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,25 @@ const gulp      = require('gulp'),
     autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function(){ 
-	return gulp.src('./src/scss/**/*.scss') 
+	return gulp.src('./public/static/scss/**/*.scss') 
 		.pipe(sass()) 
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-		.pipe(gulp.dest('./src/css/')) 
+		.pipe(gulp.dest('./public/static/css/')) 
 		.pipe(browserSync.reload({stream: true}));
 });
  
 gulp.task('browser-sync', function() { 
 	browserSync({ 
 		server: { 
-			baseDir: './src' 
+			baseDir: './public' 
 		},
 		notify: false 
 	});
 });
  
 gulp.task('watch', function() {
-	gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass')); 
-    gulp.watch('./src/*.html', browserSync.reload); 
+	gulp.watch('./public/static/scss/**/*.scss', gulp.parallel('sass')); 
+    gulp.watch('./public/static/*.html', browserSync.reload); 
 });
 gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
 
